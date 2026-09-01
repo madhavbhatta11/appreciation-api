5ac23ef7f79abe0cb1573fe4ade82fcd2b670dd3
madhavbhatta11 <madhavbhatta11@gmail.com>
2026-09-01 14:29:44 +0545

    Configured database URL with environment settings

--- app/config.py
+++ app/config.py
@@ -1,8 +1,14 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+
     database_url: str = "sqlite:///./appreciation.db"
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+    )
 
 
 settings = Settings()
